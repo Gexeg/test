ababc2359ccc98f62bfd54e9d4af56f11ff5c308
Gexeg <psiondnd@yandex.ru>
2019-03-10 10:09:10 +0500

    закончил ExecutorGenerator и TaskGenerator

---
+++ round_robin/round_robin/executor_generator.py
@@ -0,0 +1,105 @@
+import requests
+from random import randint
+import unittest
+
+
+class Executor:
+    def __init__(self, name, productivity):
+        self.__name = name
+        self.__productivity = productivity
+        self.__task_queue = []
+
+    def get_name(self):
+        return self.__name
+
+    def get_productivity(self):
+        return self.__productivity
+
+    def look_current_task(self):
+        return self.__task_queue[-1]
+
+    def add_new_task(self, task):
+        self.__task_queue.insert(0, task)
+
+    def complete_task(self):
+        self.__task_queue.pop()
+
+
+class ExecutorGenerator:
+    def __init__(self, min_productivity, max_productivity):
+        self.__min_productivity = min_productivity
+        self.__max_productivity = max_productivity
+        self.__url_for_requests = 'http://names.drycodes.com/'
+        self.__param_for_url_boy_names = '?nameOptions=boy_names'
+        self.__param_for_url_girl_names = '?nameOptions=girl_names'
+
+    def get_executors_roster(self,number_of_names, num_boy_names=None):
+        executor_names = self.__generate_names(number_of_names, num_boy_names)
+        executors = []
+        for name in executor_names:
+            new_executor_productivity = self.__generate_productivity()
+            new_executor = Executor(name, new_executor_productivity)
+            executors.append(new_executor)
+        return executors
+
+    def set_min_max_productivity(self, min_productivity, max_productivity):
+        self.__min_productivity = min_productivity
+        self.__max_productivity = max_productivity
+
+    def __generate_names(self, number_of_names, num_boy_names=None):
+        num_boy_names = randint(0, number_of_names) if num_boy_names is None else num_boy_names
+        num_girl_names = number_of_names - num_boy_names if num_boy_names != number_of_names else -1
+        all_names = self.__generate_boy_names(num_boy_names) + self.__generate_girl_names(num_girl_names)
+        result = []
+        for name in all_names:
+            if name not in result:
+                result.append(name)
+            else:
+                name_counter = 2
+                name_with_counter = name + '_' + str(name_counter)
+                while name_with_counter in result:
+                    name_counter += 1
+                    name_with_counter = name + '_' + str(name_counter)
+                result.append(name_with_counter)
+        return result
+
+    def __generate_productivity(self):
+        return randint(self.__min_productivity, self.__max_productivity)
+
+    def __generate_boy_names(self, number_of_names):
+        url_for_request = self.__url_for_requests + str(number_of_names) + self.__param_for_url_boy_names
+        name_roster = requests.get(url_for_request)
+        if name_roster.ok:
+            return name_roster.json()
+        else:
+            name_roster = self.__offline_generate_boy_names(number_of_names)
+            return name_roster
+
+    def __offline_generate_boy_names(self, number_of_names):
+        name_roster = ['boy_name' + str(i+1) for i in range(number_of_names)]
+        return name_roster
+
+    def __generate_girl_names(self, number_of_names):
+        url_for_request = self.__url_for_requests + str(number_of_names) + self.__param_for_url_girl_names
+        name_roster = requests.get(url_for_request)
+        if name_roster.ok:
+            return name_roster.json()
+        else:
+            name_roster = self.__offline_generate_girl_names(number_of_names)
+            return name_roster
+
+    def __offline_generate_girl_names(self, number_of_names):
+        name_roster = ['girl_name' + str(i+1) for i in range(number_of_names)]
+        return name_roster
+
+
+
+class ls2_test(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+
+
+    def tearDown(self):
+        pass
---
+++ round_robin/round_robin/task_generator.py
@@ -0,0 +1,62 @@
+from random import choice, randint
+
+
+class Task:
+    def __init__(self, name, complexity):
+        self.__name = name
+        self.__complexity = complexity
+
+    def get_name(self):
+        return self.__name
+
+    def get_complexity(self):
+        return self.__complexity
+
+    def reduce_complexity(self, executor_productivity):
+        self.__complexity = self.__complexity - executor_productivity
+
+    def check_is_complete(self):
+        return self.__complexity <= 0
+
+
+class TaskGenerator:
+    def __init__(self, min_complexity, max_complexity):
+        self.__min_complexity = min_complexity
+        self.__max_complexity = max_complexity
+        self.__task_adverb = ['Быстро ', 'Феерично ', 'Дорого ', 'Эффектно ', 'Похрюкивая ',
+                              'Дешево ', 'К заходу солнца ', 'В течение недели ', 'В течение суток ', 'Завтра ']
+        self.__task_verb = ['починить ', 'сломать ', 'взломать ', 'вскрыть ', 'найти ', 'арендовать ']
+        self.__task_noun = ['сервер', 'сарай', 'сейф', 'шкаф', 'скрипт', 'склад', 'смартфон', 'лэптоп']
+
+    def generate_tasks(self, number_of_tasks):
+        task_name_roster = self.__generate_task_names(number_of_tasks)
+        tasks = []
+        for name in task_name_roster:
+            new_task_complexity = self.__generate_task_complexity()
+            new_task = Task(name, new_task_complexity)
+            tasks.append(new_task)
+        return tasks
+
+    def set_min_max_complexity(self,min_complexity, max_complexity):
+        self.__min_complexity = min_complexity
+        self.__max_complexity = max_complexity
+
+    def __generate_task_names(self, number_of_tasks):
+        task_name_roster = []
+        for i in range(number_of_tasks):
+            task_name = choice(self.__task_adverb) + choice(self.__task_verb) + choice(self.__task_noun)
+            if task_name not in task_name_roster:
+                task_name_roster.append(task_name)
+            else:
+                task_counter = 2
+                task_name_with_counter = task_name + '_' + str(task_counter)
+                while task_name_with_counter in task_name_roster:
+                    task_counter += 1
+                    task_name_with_counter = task_name + '_' + str(task_counter)
+                task_name_roster.append(task_name_with_counter)
+        return task_name_roster
+
+    def __generate_task_complexity(self):
+        return randint(self.__min_complexity, self.__max_complexity)
+
+
